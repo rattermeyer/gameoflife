b6621860ec58285c68c7bc9f7aa83fd55a5e1b30
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2022-01-28 13:29:26 +0000

    initial commit

---
+++ src/test/java/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+package gameoflife;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class GameOfLifeTest {
+    @Test
+    void shouldDoSomething() {
+        fail("oh no!");
+    }
+}
+
+
