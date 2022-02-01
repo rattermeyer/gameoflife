520d5de49e8d8cc4d236f4ac2cac06e90921f61f
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2022-02-01 07:49:11 +0000

    solution example

--- src/gol.spec.ts
+++ src/gol.spec.ts
@@ -1,5 +1,132 @@
-import { GoL } from './gol'
+import {GoL} from './gol'
 
-test('to be filled', () => {
-  throw new Error('not implemented yet')
+test('we can initialize the board correctly', () => {
+    let startingBoard = [
+        [0, 0, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 0, 0],
+        [0, 0, 1, 1]
+    ];
+    expect(new GoL(startingBoard).board).toEqual(startingBoard)
+})
+
+test('when there is only one life cell in the middle, it dies by underpopulation', () => {
+    // Arrange
+    const startingBoard = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+    ]
+    const nextStepBoard = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    let game = new GoL(startingBoard);
+    // Act
+    game.step()
+    // Assure
+    expect(game.board).toEqual(nextStepBoard)
+})
+
+test('counts the neighbors of middle cell correctly', () => {
+    // Arrange
+    const startingBoard = [
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 0, 0],
+    ]
+    let goL = new GoL(startingBoard);
+    // ACT + Assure
+    expect(goL.countNeighbors(1,1)).toEqual(2)
+})
+
+test('counts the neighbors of an edge cell correctly', () => {
+    // Arrange
+    const startingBoard = [
+        [1, 1, 1],
+        [0, 1, 0]
+    ]
+    let goL = new GoL(startingBoard);
+    // ACT + Assure
+    expect(goL.countNeighbors(0,0)).toEqual(2)
+    expect(goL.countNeighbors(1,0)).toEqual(3)
+    expect(goL.countNeighbors(2,0)).toEqual(2)
+    expect(goL.countNeighbors(0,1)).toEqual(3)
+    expect(goL.countNeighbors(1,1)).toEqual(3)
+    expect(goL.countNeighbors(2,1)).toEqual(3)
+})
+
+test('when there is one live cell with two live neighbors, it survives', () => {
+    // Arrange
+    const startingBoard = [
+        [1, 0, 1],
+        [0, 1, 0]
+    ]
+    const nextStepBoard = [
+        [0, 1, 0],
+        [0, 1, 0]
+    ]
+    const game = new GoL(startingBoard)
+    // ACT
+    game.step()
+    // Assure
+    expect(game.board).toEqual(nextStepBoard)
+
+})
+
+test('when there is one live cell with three live neighbors, it survives', () => {
+    // Arrange
+    const startingBoard = [
+        [1, 1, 1],
+        [0, 1, 0]
+    ]
+    const nextStepBoard = [
+        [1, 1, 1],
+        [1, 1, 1]
+    ]
+    const game = new GoL(startingBoard)
+    // ACT
+    game.step()
+    // Assure
+    expect(game.board).toEqual(nextStepBoard)
+
+})
+
+test('when there is one live cell with four live neighbors, it dies', () => {
+    // Arrange
+    const startingBoard = [
+        [1, 1, 1],
+        [0, 1, 0],
+        [1, 1, 0]
+    ]
+    const nextStepBoard = [
+        [1, 1, 1],
+        [0, 0, 0],
+        [1, 1, 0],
+    ]
+    const game = new GoL(startingBoard)
+    // ACT
+    game.step()
+    // Assure
+    expect(game.board).toEqual(nextStepBoard)
+})
+
+test('when there is one dead cell with exactly three live neighbors, it becomes alive', () => {
+    // Arrange
+    const startingBoard = [
+        [1, 0, 1],
+        [0, 0, 0],
+        [0, 1, 0]
+    ]
+    const nextStepBoard = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+    ]
+    const game = new GoL(startingBoard)
+    // ACT
+    game.step()
+    // Assure
+    expect(game.board).toEqual(nextStepBoard)
 })
--- src/gol.ts
+++ src/gol.ts
@@ -1,2 +1,31 @@
 export class GoL {
+    board: number[][];
+
+    constructor(startingBoard: number[][]) {
+        this.board = startingBoard
+    }
+
+
+    countNeighbors(posX: number, posY: number): number {
+        let count = 0;
+        for (let x = posX - 1; x <= posX + 1; x++) {
+            for (let y = posY - 1; y <= posY + 1; y++) {
+                if ((x === posX) && (y === posY)) continue
+                count += (this.board[y]?.[x] || 0)
+            }
+        }
+        return count
+    }
+
+    step() {
+        this.board = this.board.map((row, posY) => {
+            return row.map((cell, posX) => {
+                const countNeighbors = this.countNeighbors(posX, posY);
+                if (cell === 1 && [2,3].includes(countNeighbors)) return 1
+                if (cell === 0 && countNeighbors === 3) return 1
+                return 0
+            })
+        })
+
+    }
 }
