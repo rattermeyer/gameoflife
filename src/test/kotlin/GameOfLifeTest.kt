bb9d9aa138498ef6e05764ea0b3c1215c8b938e5
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2022-02-15 12:52:58 +0000

    sample solution

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -1,5 +1,7 @@
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class GameOfLifeTest {
@@ -11,22 +13,90 @@ class GameOfLifeTest {
 
     @Test
     fun `a cell with no neighbors dies`() {
-        val cells = setOf(Cell(1,1));
+        val cells = setOf(Cell(1, 1));
         val nextGen = Board(cells).step()
-        nextGen.shouldBeEmpty()
+        nextGen.cellsAlive.shouldBeEmpty()
     }
 
     @Test
-    fun `a live cell with two neighbors survives` () {
-        val board = Board(setOf(Cell(0,0), Cell(2,0), Cell(1,1)))
-        board.step().shouldContain(Cell(1,1));
+    fun `a live cell with two neighbors survives`() {
+        val board = Board(setOf(Cell(0, 0), Cell(2, 0), Cell(1, 1)))
+        board.step().cellsAlive.shouldContain(Cell(1, 1));
+    }
+    @Test
+    fun `a live cell with three neighbors survives`() {
+        val board = Board(setOf(Cell(0, 0), Cell(1,0), Cell(2, 0), Cell(1, 1)))
+        board.step().cellsAlive.shouldContain(Cell(1, 1));
+    }
+    @Test
+    fun `a live cell with more than three neighbors dies`() {
+        val board = Board(setOf(Cell(0, 0), Cell(1,0), Cell(2, 0), Cell(1, 1), Cell(0,2)))
+        board.step().cellsAlive.shouldNotContain(Cell(1, 1));
+    }
+    @Test
+    fun `a dead cell with exactly three neighbors becomes alive`() {
+        val board = Board(setOf(Cell(0, 0), Cell(2, 0), Cell(0,2)))
+        board.step().cellsAlive.shouldContain(Cell(1, 1));
     }
 
-    class Cell(posX: Int, posY: Int)
+    @Test
+    fun `a cell with no neighbors has a count of neighbors of 0`() {
+        val board = Board(setOf(Cell(1, 1)))
+        board.countNeighbors(Cell(1, 1)).shouldBe(0)
+    }
+
+    @Test
+    fun `a cell with one neighbor has a count of neighbors of 1`() {
+        val board = Board(setOf(Cell(0, 0), Cell(1, 1)))
+        board.countNeighbors(Cell(1, 1)).shouldBe(1)
+    }
+
+
+    data class Cell(val posX: Int, val posY: Int) {
+        override fun toString(): String {
+            return "Cell(posX=$posX, posY=$posY)"
+        }
+
+    }
+
     class Board(val cellsAlive: Set<Cell>) {
-        fun step(): Set<Cell> {
-            cellsAlive.stream()
-            return setOf()
+        fun step(): Board {
+            val nextGen = mutableSetOf<Cell>()
+            cellsAlive.forEach { it ->
+                val countNeighbors = countNeighbors(it)
+                if (countNeighbors == 2 || countNeighbors == 3) {
+                    nextGen.add(it)
+                }
+                testDeadNeighboringCells(it, nextGen)
+            }
+            return Board(nextGen)
+        }
+
+        private fun testDeadNeighboringCells(
+            it: Cell,
+            nextGen: MutableSet<Cell>
+        ) {
+            for (dx in -1..1) {
+                for (dy in -1..1) {
+                    val testingCell = Cell(it.posX + dx, it.posY + dy)
+                    if (countNeighbors(testingCell) == 3) {
+                        nextGen.add(testingCell)
+                    }
+                }
+            }
+        }
+
+        fun countNeighbors(cell: Cell): Int {
+            var count = 0
+            for (dx in -1..1) {
+                for (dy in -1..1) {
+                    val element = Cell(cell.posX + dx, cell.posY + dy)
+                    if (cellsAlive.contains(element) && element != cell) {
+                        count++
+                    }
+                }
+            }
+            return count
         }
     }
 }
