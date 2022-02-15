81906ad2bf0bc5fe0027cb9f88c8c3e8b9e2128e
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2022-02-15 12:56:44 +0000

    kotlin starting point

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -1,32 +1,9 @@
-import io.kotest.matchers.collections.shouldBeEmpty
-import io.kotest.matchers.collections.shouldContain
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 
 class GameOfLifeTest {
     @Test
-    fun `can construct an empty board`() {
-        val board = Board(setOf())
-        board.cellsAlive.shouldBeEmpty()
-    }
-
-    @Test
-    fun `a cell with no neighbors dies`() {
-        val cells = setOf(Cell(1,1));
-        val nextGen = Board(cells).step()
-        nextGen.shouldBeEmpty()
-    }
-
-    @Test
-    fun `a live cell with two neighbors survives` () {
-        val board = Board(setOf(Cell(0,0), Cell(2,0), Cell(1,1)))
-        board.step().shouldContain(Cell(1,1));
-    }
-
-    class Cell(posX: Int, posY: Int)
-    class Board(val cellsAlive: Set<Cell>) {
-        fun step(): Set<Cell> {
-            cellsAlive.stream()
-            return setOf()
-        }
+    fun `some test`() {
+        fail("not implemented yet")
     }
 }
